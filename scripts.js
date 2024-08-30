5464614b1b77966acb9131c6ffc5af1eb347de78
Anh La <37478019+Anh-La@users.noreply.github.com>
2024-08-31 03:07:30 +0930

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,6 +7,20 @@
 // Scripts
 // 
 
+ /* Running text in top navbar - Text box marquee            */
+speed_Scrolling = 150
+characters_Scroll = 1
+
+function scrolling_Text() {
+    window.setTimeout('scrolling_Text()', speed_Scrolling);
+
+    var msg = document.text_Marquee.text.value;
+    document.text_Marquee.text.value =
+        msg.substring(characters_Scroll) +
+        msg.substring(0, characters_Scroll);
+}
+scrolling_Text()
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Toggle the side navigation
