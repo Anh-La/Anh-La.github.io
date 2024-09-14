6cb7925e00c6073b183e474e4f07977050e7a69f
Anh La <37478019+Anh-La@users.noreply.github.com>
2024-09-14 13:22:21 +0930

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -44,7 +44,7 @@ window.addEventListener('DOMContentLoaded', event => {
     // Toggle the side navigation
     const sidebarToggle = document.body.querySelector('#sidebarToggle');
     // Apply the sb-sidenav-toggled class by default
-    document.body.classList.add('sb-sidenav-toggled');
+    //document.body.classList.add('sb-sidenav-toggled');
     
     if (sidebarToggle) {
         // Persist sidebar toggle between refreshes
