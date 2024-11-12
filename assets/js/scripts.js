b84da8b5b10ba8693f615cdcdbd1a0f1d7806baa
Anh La <37478019+Anh-La@users.noreply.github.com>
2024-11-13 05:13:10 +0930

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -79,23 +79,23 @@ $(document).ready(function() {
 });
 
  // Tab pages setup
-function openPage(pageName, elmnt) {
+//function openPage(pageName, elmnt) {
     // Hide all elements with class="tabcontent" by default
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+    //var i, tabcontent, tablinks;
+    //tabcontent = document.getElementsByClassName("tabcontent");
+    //for (i = 0; i < tabcontent.length; i++) {
+        //tabcontent[i].style.display = "none";
+    //}
     // Remove the background color of all tablinks/buttons
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].classList.remove('active');
-    }
+    //tablinks = document.getElementsByClassName("tablink");
+    //for (i = 0; i < tablinks.length; i++) {
+        //tablinks[i].classList.remove('active');
+    //}
     // Show the specific tab content
-    document.getElementById(pageName).style.display = "block";
+    //document.getElementById(pageName).style.display = "block";
     // Add the active class to the current button
-    elmnt.classList.add('active');
-}
+    //elmnt.classList.add('active');
+//}
 
 
 // Get the default tab open
