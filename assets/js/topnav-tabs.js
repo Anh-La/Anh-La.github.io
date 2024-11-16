f882ec0eb347a0614c923b7f60566fc6d345020a
Anh La <37478019+Anh-La@users.noreply.github.com>
2024-11-17 05:14:26 +0930

    Create topnav-tabs.js

---
+++ assets/js/topnav-tabs.js
@@ -0,0 +1,23 @@
+
+ // Tab pages setup
+//function openPage(pageName, elmnt) {
+    // Hide all elements with class="tabcontent" by default
+    //var i, tabcontent, tablinks;
+    //tabcontent = document.getElementsByClassName("tabcontent");
+    //for (i = 0; i < tabcontent.length; i++) {
+        //tabcontent[i].style.display = "none";
+    //}
+    // Remove the background color of all tablinks/buttons
+    //tablinks = document.getElementsByClassName("tablink");
+    //for (i = 0; i < tablinks.length; i++) {
+        //tablinks[i].classList.remove('active');
+    //}
+    // Show the specific tab content
+    //document.getElementById(pageName).style.display = "block";
+    // Add the active class to the current button
+    //elmnt.classList.add('active');
+//}
+
+
+// Get the default tab open
+//document.getElementById("homeTab").click();
